b8f87135dc05033a1b060d526ec587123e73e6ad
Wandalen <wandalen.me@gmail.com>
2018-12-27 10:53:05 +0200

    auto commit

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,7 +1,13 @@
 
-let _ = require( 'wTools' );
+let Stager = require( 'wstager' );
 
-/**/
+/* */
 
-...
-
+let stager = new _.Stager
+({
+  object : {},
+  stageNames : [ 'formed', 'willFilesFound', 'willFilesOpened', 'resourcesFormed' ],
+  consequenceNames : [ 'formReady', 'willFilesFindReady', 'willFilesOpenReady', 'resourcesFormReady' ],
+  finals : [ 3, 2, 2, 2 ],
+  verbosity : 1,
+});
